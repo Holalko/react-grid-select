aa73018cb1226214a872e859dd01118ed6b18c8a
Jakub Holaza <jakub.holaza@notino.com>
2021-12-11 11:08:07 +0100

    change line separator

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,3 @@
-export function range(size:number):ReadonlyArray<number> {    return Array.from({length: size}, (x, i) => i);}
+export function range(size:number):ReadonlyArray<number> {
+    return Array.from({length: size}, (x, i) => i);
+}
